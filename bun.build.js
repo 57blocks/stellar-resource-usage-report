b9fafdae35a63be9543ddfde7a5ce41572f1bc14
songfintary <139731563+songfintary@users.noreply.github.com>
2024-12-06 14:49:08 +0800

    chore: update npm to bun

---
+++ bun.build.js
@@ -0,0 +1,15 @@
+const results = await Bun.build({
+  entrypoints: ['./src/main.ts'],
+  publicPath: '',
+  outdir: './dist',
+  sourcemap: 'inline',
+});
+
+if (results.success == false) {
+  console.error('Build failed');
+  for (const message of results.logs) {
+    console.error(message);
+  }
+} else {
+  console.log('Compiled ' + results.outputs.length + ' javascript files...');
+}
