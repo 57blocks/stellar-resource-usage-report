d09e140eab3bff4104eae449c0fb57b52c6d43a2
songfintary <139731563+songfintary@users.noreply.github.com>
2024-12-19 10:12:33 +0800

    feat: add legend for table

--- src/share/index.ts
+++ src/share/index.ts
@@ -5,7 +5,51 @@ import { STELLAR_LIMITS_CURSORS } from '@/types/enums';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const style = require('tty-table/src/style');
 
+const printTableInfo = () => {
+  const tableConfig: Table.Options = {
+    borderStyle: 'dashed',
+    color: 'green',
+    borderColor: 'yellowBright',
+  };
+
+  console.log(
+    Table(
+      [
+        [
+          style.style(
+            '===================================== Resource Usage Table =====================================',
+            'cyanBright'
+          ),
+        ],
+      ],
+      {
+        borderStyle: 'none',
+        align: 'center',
+      } as Table.Options
+    ).render()
+  );
+
+  const rows = [
+    [
+      style.style(`Success ✅ : [0, ${STELLAR_LIMITS_CURSORS.WARNING * 100}%]`, 'bgGreen'),
+      style.style(
+        `Warning: (${STELLAR_LIMITS_CURSORS.WARNING * 100}%, ${STELLAR_LIMITS_CURSORS.DANGER * 100}%]`,
+        'bgYellow'
+      ),
+      style.style(
+        `Danger: (${STELLAR_LIMITS_CURSORS.DANGER * 100}%, ${STELLAR_LIMITS_CURSORS.ERROR * 100}%]`,
+        'bgMagenta'
+      ),
+      style.style(`Error: (${STELLAR_LIMITS_CURSORS.ERROR * 100}%, ]`, 'bgRed'),
+    ],
+  ];
+
+  const t3 = Table(rows, tableConfig);
+  console.log(t3.render());
+};
+
 export const printTable = (rows: (string | number)[][]) => {
+  printTableInfo();
   const tableConfig: Table.Options = {
     borderStyle: 'solid',
     color: 'green',
@@ -14,12 +58,12 @@ export const printTable = (rows: (string | number)[][]) => {
   };
 
   const headers: Table.Header[] = [
-    { value: 'Resource', width: 35, headerColor: 'cyanBright', align: 'right' },
-    { value: 'Usage', width: 35, headerColor: 'cyanBright' },
-    { value: 'Limit', width: 40, headerColor: 'cyanBright' },
+    { value: 'Resource', width: 25, headerColor: 'cyanBright', align: 'right' },
+    { value: 'Usage', width: 25, headerColor: 'cyanBright' },
+    { value: 'Limit', width: 25, headerColor: 'cyanBright' },
     {
       value: 'Result',
-      width: 30,
+      width: 25,
       headerColor: 'cyanBright',
       formatter: (_cellValue, _columnIndex, rowIndex, rowData) => {
         const [_key, _value, _limit, _percent] = rowData[rowIndex];
