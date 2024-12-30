615c2da672e75a45d741bc87c33c52f9cb96facd
zewei.zhang <zewei@57blocks.com>
2024-12-30 15:43:27 +0800

    feat: store the result for calculating

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,14 @@ const calcResource = async (props: CalcResourceProps) => {
 
 export function ResourceUsageClient(Client: any, options: ClientOptions) {
   class ResourceUsage extends Client {
+    contractId: string;
+    storedStatus: {
+      [functionName: string]: any[];
+    } = {};
     constructor() {
+      if (!options.contractId) {
+        throw new Error('contractId is required');
+      }
       super(options);
       const contractFunNames = this.spec.funcs().map((fun: any) => fun.name().toString());
       for (const funName of contractFunNames) {
@@ -33,22 +40,31 @@ export function ResourceUsageClient(Client: any, options: ClientOptions) {
             const assembledTx = await originalFun(...args);
             const res = await assembledTx.signAndSend();
             const stats = (await handleTxToGetStatsV2(assembledTx as any, res.getTransactionResponse as any)) as anyObj;
+            if (!this.storedStatus[funName]) {
+              // init array
+              this.storedStatus[funName] = [stats];
+            } else {
+              this.storedStatus[funName].push(stats);
+            }
+            // const lis: (string | number)[][] = [];
 
-            const lis: (string | number)[][] = [];
+            // Object.entries(stats).forEach(([key, value]) => {
+            //   const limit = STELLAR_LIMITS_CONFIG[key as 'cpu_insns'];
+            //   const percent = parseFloat(((value / limit.value) * 100).toFixed(2));
+            //   lis.push([key, value, limit.label, percent]);
+            // });
 
-            Object.entries(stats).forEach(([key, value]) => {
-              const limit = STELLAR_LIMITS_CONFIG[key as 'cpu_insns'];
-              const percent = parseFloat(((value / limit.value) * 100).toFixed(2));
-              lis.push([key, value, limit.lable, percent]);
-            });
-
-            printTable(lis);
+            // printTable(lis);
             return assembledTx;
           } catch (error) {
             console.error(error);
           }
         };
       }
+      this.contractId = options.contractId;
+    }
+    printTable() {
+      console.log(this.storedStatus);
     }
   }
   return new ResourceUsage();
